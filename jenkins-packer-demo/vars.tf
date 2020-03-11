cae118ac77d4aeb9fc35ddb3326468105f427899
vitale mazo <vmazo@novus.com>
2020-03-11 15:45:57 -0500

    added s3 bucket to configure script to copy local state to s3 bucket

--- jenkins-packer-demo/vars.tf
+++ jenkins-packer-demo/vars.tf
@@ -13,8 +13,6 @@ variable "PATH_TO_PUBLIC_KEY" {
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-13be557e"
-    us-west-2 = "ami-06b94666"
     us-east-2 = "ami-0fc20dd1da406780b"
   }
 }

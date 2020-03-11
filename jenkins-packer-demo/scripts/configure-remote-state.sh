f154010824392936451354985ba952887c8f5985
vitale mazo <vmazo@novus.com>
2020-03-11 13:45:49 -0500

    added s3 bucket to configure script to copy local state to s3 bucket

--- jenkins-packer-demo/scripts/configure-remote-state.sh
+++ jenkins-packer-demo/scripts/configure-remote-state.sh
@@ -6,7 +6,7 @@ echo 'backend.tf
 ==========
 terraform {
   backend "s3" {
-    bucket = "terraform-state-a3c731f"
+    bucket = "terraform-state-a2b232621f"
     key    = "terraform.tfstate"
     region = "us-east-2"
   }
